d19148a54674cab6f67141b487eefffe071a6a2e
SunilB565 <122293473+SunilB565@users.noreply.github.com>
2025-07-19 00:32:43 +0530

    Update main.tf

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -68,7 +68,10 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app.arn
   port              = 80
   protocol          = "HTTP"
-  default_action { type = "forward"; target_group_arn = aws_lb_target_group.ecs_tg.arn }
+  default_action { 
+    type = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn 
+  }
 }
 
 resource "aws_ecs_service" "app" {
