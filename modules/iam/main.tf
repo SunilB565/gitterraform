c2080f7768fc76152fbacbb4d5c3fb281faa6f3a
SunilB565 <122293473+SunilB565@users.noreply.github.com>
2025-07-19 02:14:57 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecs_exec" {
-  name = "ecsTaskExecutionRole-${var.environment}"
+  name = "ecsTaskExecutionRole-${var.env}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17", Statement = [{
       Effect    = "Allow"
