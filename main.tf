4f66d23716a5c1121afcf1af0655f9d4a997e100
SunilB565 <122293473+SunilB565@users.noreply.github.com>
2025-07-19 00:37:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,13 +48,14 @@ resource "aws_route_table" "public" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
     Name = "public-rt"
   }
 }
+
 
 resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.public_a.id
