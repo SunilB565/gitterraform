1e1d7319993413b09030463b70b0d7693abcc171
SunilB565 <122293473+SunilB565@users.noreply.github.com>
2025-07-19 01:15:29 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -14,3 +14,6 @@ resource "aws_iam_role_policy_attachment" "ecs" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+output "ecs_exec_role_arn" {
+  value = aws_iam_role.ecs_exec.arn
+}
