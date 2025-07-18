f37827bdd2240ae9379f9449f5ad0313c23febd8
SunilB565 <122293473+SunilB565@users.noreply.github.com>
2025-07-19 00:51:14 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -20,7 +20,10 @@ resource "aws_internet_gateway" "igw" { vpc_id = aws_vpc.main.id }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0"; gateway_id = aws_internet_gateway.igw.id }
+  route { 
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id 
+  }
 }
 
 resource "aws_route_table_association" "a" {
@@ -32,8 +35,18 @@ resource "aws_route_table_association" "a" {
 resource "aws_security_group" "lb" {
   name   = "lb-sg"
   vpc_id = aws_vpc.main.id
-  ingress { from_port = 80; to_port = 80; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
-  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
+  ingress { 
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  egress { 
+    from_port = 0
+    to_port = 0 
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
 }
 
 resource "aws_security_group" "service" {
@@ -45,6 +58,11 @@ resource "aws_security_group" "service" {
     protocol        = "tcp"
     security_groups = [aws_security_group.lb.id]
   }
-  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
+  egress { 
+    from_port = 0 
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
 }
 
