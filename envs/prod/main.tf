bb3b90d22f4e1df7bee62f03be6dd636c7f45691
SunilB565 <122293473+SunilB565@users.noreply.github.com>
2025-07-19 00:57:56 +0530

    Update main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -10,7 +10,10 @@ terraform {
 
 provider "aws" { region = "us-east-1" }
 
-module "vpc" { source = "../../modules/vpc"; env = "prod" }
+module "vpc" { 
+  source = "../../modules/vpc"
+  env = "prod" 
+}
 module "iam" { source = "../../modules/iam" }
 module "ecs" {
   source              = "../../modules/ecs"
