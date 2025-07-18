22f82fb638e125514eae0f3d3837f4a533c8eed5
SunilB565 <122293473+SunilB565@users.noreply.github.com>
2025-07-19 01:05:35 +0530

    Update main.tf

--- envs/staging/main.tf
+++ envs/staging/main.tf
@@ -3,8 +3,6 @@ terraform {
     bucket         = "zptfstate001"
     key            = "staging/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
   }
 }
 
